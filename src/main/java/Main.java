1a05a7a2bed61e506860fdc86cb50edecb6da337
MeisterSky <sheff.victor@gmail.com>
2019-12-17 19:50:44 +0300

    Добавил Gradle

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+package Lesson1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("??????? ???-??");
+
+        System.out.println("");
+        System.out.println("фывыфвфыв");
+        System.out.println("                         |");
+        System.out.println("                         |                                  |");
+        System.out.println("_________________________|__________________________________|________________________________________________||Finish||");
+
+    }
+}
